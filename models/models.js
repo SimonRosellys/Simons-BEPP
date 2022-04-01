04e027f9aa3cb755b6a4a9a8790b985d9426dc11
Simon Rosellys <simonrosellys@aol.com>
2022-04-01 11:29:43 +0100

    Complete GET /api/articles/:article_id (with comment count)

--- models/models.js
+++ models/models.js
@@ -1,4 +1,6 @@
+const res = require("express/lib/response");
 const db = require("../db/connection");
+const articles = require("../db/data/test-data/articles");
 
 exports.fetchTopicsMod = () => {
   return db.query(`SELECT * FROM topics;`).then((topics) => {
@@ -8,7 +10,16 @@ exports.fetchTopicsMod = () => {
 
 exports.fetchArticleMod = (article_id) => {
   return db
-    .query(`SELECT * FROM articles WHERE article_id = $1;`, [article_id])
+    .query(
+      `
+SELECT articles.*, COUNT(comments.comment_id) AS comment_count
+FROM articles
+LEFT JOIN comments ON articles.article_id = comments.article_id
+WHERE articles.article_id = $1
+GROUP BY articles.article_id;
+`,
+      [article_id]
+    )
     .then((article) => {
       if (article.rows.length === 0) {
         return Promise.reject({ status: 404, msg: "Not found" });
@@ -41,6 +52,6 @@ exports.fetchUsersMod = () => {
     users.rows.forEach((user) => {
       usernameArr.push({ username: user.username });
     });
-    return usernameArr;
+    return { users: usernameArr };
   });
 };
